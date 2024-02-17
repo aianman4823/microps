edf3a745dcc4460f39b013212ea0e4ff719f1868
原田明斗 <harada.akito@haradaakiratonoMacBook-Pro.local>
2024-02-17 17:42:50 +0900

    add step11

--- net.c
+++ net.c
@@ -8,6 +8,7 @@
 #include "util.h"
 #include "net.h"
 #include "ip.h"
+#include "icmp.h"
 
 struct net_protocol
 {
@@ -93,6 +94,40 @@ static int net_device_close(struct net_device *dev)
     dev->flags &= ~NET_DEVICE_FLAG_UP;
     infof("dev=%s, state=%s", dev->name, NET_DEVICE_STATE(dev));
     return 0;
+}
+
+/* NOTE: must not be call after net_run() */
+int net_device_add_iface(struct net_device *dev, struct net_iface *iface)
+{
+    struct net_iface *entry;
+
+    for (entry = dev->ifaces; entry; entry = entry->next)
+    {
+        if (entry->family == iface->family)
+        {
+            errorf("already exists, dev=%s, family=%d", dev->name, entry->family);
+            return -1;
+        }
+    }
+
+    iface->dev = dev;
+    iface->next = dev->ifaces;
+    dev->ifaces = iface;
+
+    return 0;
+}
+
+struct net_iface *net_device_get_iface(struct net_device *dev, int family)
+{
+    struct net_iface *entry;
+    for (entry = dev->ifaces; entry; entry = entry->next)
+    {
+        if (entry->family == family)
+        {
+            return entry;
+        }
+    }
+    return NULL;
 }
 
 int net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst)
@@ -243,6 +278,11 @@ int net_init(void)
     if (ip_init() == -1)
     {
         errorf("ip_init() failure");
+        return -1;
+    }
+    if (icmp_init() == -1)
+    {
+        errorf("icmp_init() failure");
         return -1;
     }
     infof("initialized");
